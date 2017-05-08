3b10fbc199a723714e34b1c259ed48dc7ea86c34
Aditya Narayan <an160264@gmail.com>
2017-05-08 21:15:12 +0530

    2nd Commit

--- app/src/main/java/com/a46inch/a46inchadmin/Loading_Screen.java
+++ app/src/main/java/com/a46inch/a46inchadmin/Loading_Screen.java
@@ -1,14 +1,23 @@
 package com.a46inch.a46inchadmin;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Loading_Screen extends AppCompatActivity {
-
+    private static int SPLASH_TIME_OUT = 4000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading__screen);
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(Loading_Screen.this, Login_Screen.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
