602765f726856158c899aeda2e7f1dcfb04aa9b5
Aditya Narayan <an160264@gmail.com>
2017-05-08 20:52:20 +0530

    Initial commit

---
+++ app/src/main/java/com/a46inch/a46inchadmin/Loading_Screen.java
@@ -0,0 +1,14 @@
+package com.a46inch.a46inchadmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Loading_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading__screen);
+
+    }
+}
