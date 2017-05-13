b54106a468fea0cdd2bd1fb79efc94a58a914a2b
Aditya Narayan <an160264@gmail.com>
2017-05-13 22:26:00 +0530

    3nd Commit

---
+++ app/src/main/java/com/a46inch/a46inchadmin/Home_Screen.java
@@ -0,0 +1,13 @@
+package com.a46inch.a46inchadmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home__screen);
+    }
+}
